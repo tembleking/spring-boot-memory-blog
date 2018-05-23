79ed2941c1b068ba00d1795f359e90c999b6f6e0
Federico Barcelona Auria <fede_rico_94@hotmail.com>
2018-05-23 12:44:03 +0200

    Add custom JMX example

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -4,39 +4,63 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.awt.Graphics2Delegate;
+
+import javax.management.*;
+import java.lang.management.ManagementFactory;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-	@RequestMapping("/greeting")
-	public Greeting greeting() {
-		return new Greeting("Hello World!");
-	}
+    private static Greeting greeting = new Greeting("Hello World!");
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @RequestMapping("/greeting")
+    public Greeting greeting() {
+        return greeting;
+    }
 
+    public static void main(String[] args) {
+        try {
+            MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+            ObjectName name = new ObjectName("app:greeting=HelloWorld");
+            mBeanServer.registerMBean(DemoApplication.greeting, name);
+        } catch (MalformedObjectNameException e) {
+            e.printStackTrace();
+        } catch (NotCompliantMBeanException e) {
+            e.printStackTrace();
+        } catch (InstanceAlreadyExistsException e) {
+            e.printStackTrace();
+        } catch (MBeanRegistrationException e) {
+            e.printStackTrace();
+        }
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
 
-class Greeting {
-	private String message;
+class Greeting implements GreetingMBean {
+    private String message;
+    private int numberOfGreetings;
 
-	Greeting() {
-	}
+    Greeting() {
+    }
 
-	public Greeting(String message) {
-		super();
-		this.message = message;
-	}
+    public Greeting(String message) {
+        super();
+        this.message = message;
+    }
 
-	public String getMessage() {
-		return this.message;
-	}
+    public String getMessage() {
+        numberOfGreetings += 1;
+        return this.message;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
+    @Override
+    public int getNumberOfGreetings() {
+        return numberOfGreetings;
+    }
 }
---
+++ demo/src/main/java/com/example/GreetingMBean.java
@@ -0,0 +1,5 @@
+package com.example;
+
+public interface GreetingMBean {
+    int getNumberOfGreetings();
+}
